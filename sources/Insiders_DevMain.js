1276e3cff3635b26716f6bca4e05b8a19f28ff02
ItzLevvie <rsajeey@hotmail.com>
2020-01-01 14:25:25 +0000

    Merge branch 'staging_qa' into master

--- sources/Insiders_DevMain.js
+++ sources/Insiders_DevMain.js
@@ -3,7 +3,7 @@
 const https = require("https");
 
 const getFFN = "5440fd1f-7ecb-4221-8110-145efaa6372f";
-const getBuild = "16.0.12410.20000";
+const getBuild = "16.0.12425.20000";
 
 https.get(`https://mrodevicemgr.edog.officeapps.live.com/mrodevicemgrsvc/api/v2/C2RReleaseData?audienceFFN=${getFFN}`, (getResponse) => {
     getResponse.on("data", (getData) => {
